057b0ea894a8b95e6d1bd3cdbf8569e66f034e95
Duduxs <duduxss3@gmail.com>
2020-10-04 19:39:04 -0300

    City update and delete implemented

--- getting-started/src/main/java/org/acme/getting/started/CityResource.java
+++ getting-started/src/main/java/org/acme/getting/started/CityResource.java
@@ -3,8 +3,10 @@ package org.acme.getting.started;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,6 +14,7 @@ import javax.ws.rs.core.Response;
 
 import org.acme.getting.started.entities.City;
 import org.acme.getting.started.services.CityService;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 @Path("/cities")
 @Produces(MediaType.APPLICATION_JSON)
@@ -32,6 +35,21 @@ public class CityResource {
 	@Transactional
 	public Response persist(City city) {
 		return cityService.persist(city);
+	}
+	
+	@PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	@Transactional
+	public Response update(@PathParam("id")Long id, City city) {
+		return cityService.update(id, city);
+	}
+	
+	@DELETE
+	@Path("{id}")
+	@Transactional
+	public Response delete(@PathParam("id")Long id) {
+		return cityService.delete(id);
 	}
 
 }
--- getting-started/src/main/java/org/acme/getting/started/services/CityService.java
+++ getting-started/src/main/java/org/acme/getting/started/services/CityService.java
@@ -6,6 +6,7 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
 import org.acme.getting.started.entities.City;
+import org.acme.getting.started.entities.State;
 import org.acme.getting.started.repositories.CityRepository;
 import org.acme.getting.started.repositories.StateRepository;
 
@@ -25,10 +26,37 @@ public class CityService {
 	public Response persist(City city) {
 		if (stateRepository.findById(city.getState().getId()) == null)
 			throw new WebApplicationException("State not found!", Response.Status.NOT_FOUND);
-
+		
+		State state = stateRepository.findById(city.getState().getId());
+		
+		city.getState().setName(state.getName());
+		city.getState().setRegion(state.getRegion());
+		
 		cityRepository.persist(city);
-
 		return Response.ok(city).status(Response.Status.CREATED).build();
+	}
+	
+	public Response update(Long id, City city) {
+		City updateCity = cityRepository.findById(id);
+		
+		if(cityRepository.findById(id) == null)
+			throw new WebApplicationException("City not found!", Response.Status.NOT_FOUND);
+		
+		State state = stateRepository.findById(city.getState().getId());
+		
+		updateCity.setName(city.getName());
+		updateCity.getState().setName(state.getName());
+		updateCity.getState().setRegion(state.getRegion());
+		
+		return Response.ok(updateCity).build();
+	}
+	
+	public Response delete(Long id) {
+		if(cityRepository.findById(id) == null)
+			throw new WebApplicationException("City not found!", Response.Status.NOT_FOUND);
+		
+		cityRepository.deleteById(id);
+		return Response.noContent().build();
 	}
 
 }
